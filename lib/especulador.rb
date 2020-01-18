1bd55ec6d37c37a5b4b37aaad9fe98ae519da725
Fernando Talavera <ftm1997@correo.ugr.es>
2020-01-18 17:13:55 +0100

    Practica 4: refactor

--- lib/especulador.rb
+++ lib/especulador.rb
@@ -4,6 +4,8 @@
 
 module Civitas
   class Especulador < Jugador
+    alias :super_tiene_salvoconducto :tiene_salvoconducto
+    
     @@factorEspeculador = 2
     
     def initialize(otro, fianza)
@@ -17,29 +19,26 @@ module Civitas
     
     def to_s
       salida = "*+++*+++* Jugador Especulador *+++*+++*\n"
-      salida += super.to_s
+      salida += super
 
       return salida;
     end
     
-    
     def encarcelar(num_casilla_carcel)
-        if !super.tiene_salvoconducto()
-            if !self.pagar_fianza
-                self.mover_casilla(num_casilla_carcel);
-                self.encarcelado = true;
-                Diario.instance.ocurre_evento("El jugador ha sido encarcelado");
-            end
+        if !super_tiene_salvoconducto && !self.pagar_fianza
+          self.mover_casilla(num_casilla_carcel);
+          @encarcelado = true;
+          Diario.instance.ocurre_evento("El jugador ha sido encarcelado");
         end
 
-        return this.encarcelado
+        return @encarcelado
     end
 
     def paga_impuesto(cantidad)
         salida = false;
 
         if !this.encarcelado
-            salida = this.paga(cantidad/2)
+            salida = self.paga(cantidad/2)
         end
 
         return salida
@@ -63,36 +62,6 @@ module Civitas
         end
 
         return puede_pagar
-    end
-
-    def puedo_edificar_casa(propiedad)
-        puedo_edificar_casa = false
-
-        precio = propiedad.precio_edificar
-
-        if self.puedo_gastar(precio) 
-            if propiedad.num_casas < self.get_casa_max
-                puedo_edificar_casa = true
-            end
-        end
-
-        return puedo_edificar_casa;
-    end
-
-    def puedo_edificar_hotel(propiedad)
-        puedo_edificar_hotel = false;
-
-        precio = propiedad.precio_edificar
-
-        if this.puedo_gastar(precio)
-            if propiedad.num_hoteles < self.get_hoteles_max
-                if propiedad.num_casas >= Jugador.CasasPorHotel
-                    puedo_edificar_hotel = true
-                end
-            end
-        end
-
-        return puedo_edificar_hotel;
     end
   end
 end
